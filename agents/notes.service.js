0586d385963c32a6bc0f4c8811e8ee1cffed9704
taras.panasiuk <webdev.taras@gmail.com>
2020-04-19 21:28:38 +0300

    notes.service

---
+++ agents/notes.service.js
@@ -0,0 +1,22 @@
+const notes = require('../services/notes.service')
+
+module.exports = {
+  name: 'notes',
+
+  actions: {
+    listAll(ctx) {
+      return notes.listAll()
+    },
+    getById(ctx) {
+      const id = Number(ctx.params.id)
+      return notes.getById({ id })
+    },
+    create(ctx) {
+      return notes.create(ctx.params.payload)
+        .then(note => {
+          this.broker.emit('note.created', note)
+          return note
+        })
+    }
+  },
+}
--- services/notes.service.js
+++ services/notes.service.js
@@ -1,29 +1,25 @@
-const notes = [
-  {id: 1, title: 'note 1'},
-  {id: 2, title: 'note 2', checked: false },
-  {id: 2, title: 'note 3'},
-]
+const notes = require('../storages/notes.storage')
 
 module.exports = {
   name: 'notes',
 
-  actions: {
-    listAll(ctx) {
-      return Promise.resolve({ notes: notes })
-    },
-    getById(ctx) {
-      const id = Number(ctx.params.id)
-      return Promise.resolve(notes.find(note => note.id === id ))
-    },
-    create(ctx) {
-      const lastId = Math.max(...notes.map(note => note.id))
-      const note = {
-        id: lastId + 1,
-        ...ctx.params.payload,
-      }
-      notes.push(note)
-      this.broker.emit('note.created', note)
-      return Promise.resolve(note)
-    }
+  listAll({}) {
+    return Promise.resolve({ notes: notes.slice() })
   },
+
+  getById({ id }) {
+    return Promise.resolve(notes.find(note => note.id === id ))
+  },
+
+  create(payload) {
+    const lastId = Math.max(...notes.map(note => note.id))
+    const note = {
+      id: lastId + 1,
+      ...payload,
+    }
+    notes.push(note)
+    // TODO: hook.after.create
+    // this.broker.emit('note.created', note)
+    return Promise.resolve(note)
+  }
 }
